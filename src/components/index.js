73cf49b406729b39cdb9315f83670bd473d2a716
Kyle Glasper <ksglasper@gmail.com>
2022-11-12 14:04:40 -0600

    fixed searched name duplication

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,6 @@ export {default as App} from './App'
 export {default as MyLocation} from './MyLocation'
 export {default as Geolocation} from './Geolocation'
 export {default as Loading} from './Loading'
+export {default as WeatherSearchCard} from './WeatherSearchCard'
+
 
