26de2a25ca438b3250ca1d04faf6da9fac986757
Kyle Glasper <ksglasper@gmail.com>
2022-12-23 17:58:23 -0600

    creating problems out of thin air

--- src/components/WeatherSearchCard.jsx
+++ src/components/WeatherSearchCard.jsx
@@ -1,22 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 function WeatherSearchCard({searchWeatherResults, setIsLoading}) {
+  const [readableTime, setReadableTime] = useState('')
     const weatherObj = {...searchWeatherResults}
 
+
+    const updateTime = (timestamp)=>{
+      let date = ''
+      let time = ''
+
+      for(let i = 0; i<timestamp.length; i++){
+        date+= timestamp.charAt(i)
+        if(timestamp.charAt(i) === 'T'){
+          date = date.slice(0,i)
+          time = timestamp.slice(i+1,i+6)
+          setReadableTime(time.concat(' ' + date))
+          console.log(time, date, 'dabiys')
+        }
+      }
+    }
+    useEffect(()=>{
+      updateTime(weatherObj.time)
+  },[searchWeatherResults])
   
 
 
   return ( 
     <div className="weatherContainer">
     <Card style={{ width: '18rem' }}>
+      <Card.Header>{readableTime}</Card.Header>
       <Card.Img variant="top" src={`./images/${weatherObj.symbol}.png`} />
       <Card.Body>
         <Card.Title>{weatherObj.city ? `${weatherObj.city}, ${weatherObj.country}` : weatherObj.country}</Card.Title>
         <Card.Text>
           {weatherObj.temperature}°F
+          Feels Like {weatherObj.feelsLikeTemp}°F
+
         </Card.Text>
       </Card.Body>
       <ListGroup className="list-group-flush">
